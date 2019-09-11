c54e48665b72895b369159f858efd43b09a03f3e
Ercan-home <ercanerdogan@gmail.com>
2019-09-12 00:15:34 +0300

    initial commit

---
+++ ASPNetCoreMVC2Introduction/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNetCoreMVC2Introduction.Entities;
+using ASPNetCoreMVC2Introduction.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreMVC2Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from first Application";
+        }
+
+        public ViewResult Index2()
+        {
+            return View();
+           
+        }
+
+        public ViewResult Index3()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee{ID=1, FirstName ="Ercan", LastName ="Erdogan", CityID=35 },
+                new Employee{ID=2, FirstName ="Hakan", LastName ="Erdogan", CityID=35 }
+
+            };
+
+            List<string> cities = new List<string> { "Istanbul", "Izmir", "Ankara" };
+
+            var model = new EmployeeListViewModel
+            {
+                Employees = employees,
+                Cities = cities
+
+            };
+
+            return View(model);
+
+
+        }
+
+        public StatusCodeResult Index4()
+        {
+            return NotFound();
+        }
+
+        public StatusCodeResult Index5()
+        {
+            return Ok();
+        }
+
+        public RedirectResult Index6()
+        {
+            return Redirect("Home/Index4");
+        }
+
+        public IActionResult Index7()
+        {
+            return RedirectToAction("Index3");
+        }
+
+        public JsonResult Index8()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee{ID=1, FirstName ="Ercan", LastName ="Erdogan", CityID=35 },
+                new Employee{ID=2, FirstName ="Hakan", LastName ="Erdogan", CityID=35 }
+
+            };
+
+            return Json(employees);
+
+        }
+    }
+}
---
+++ ASPNetCoreMVC2Introduction/Models/EmployeeListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ASPNetCoreMVC2Introduction.Entities;
+
+namespace ASPNetCoreMVC2Introduction.Models
+{
+    class EmployeeListViewModel
+    {
+        public List<Employee> Employees { get; set; }
+        public List<string> Cities { get; set; }
+    }
+}
