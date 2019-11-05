41c1e9c5f7bcd80be7dbcbf774cb178647c391f1
Ercan Erdoğan <ercan.erdogan@innovile.com>
2019-11-05 18:28:10 +0300

    -bower setup

---
+++ ASPNetCoreMVC2Introduction/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreMVC2Introduction.Controllers
+{
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        [Route("")]
+        [Route("save")]
+        [Route("~/save")]
+        public string Save()
+        {
+            return "Saved";
+        }
+
+        [Route("delete/{id?}")]
+        public string Delete(int id=0)
+        {
+            return string.Format("Deleted {0}", id);
+        }
+
+        [Route("update")]
+        public string Update()
+        {
+            return "Updated";
+        }
+    }
+}
